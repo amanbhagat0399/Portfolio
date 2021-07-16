58326d00092289aa2957e009d97ffe4156d0ea83
Aman Bhagat <amanbhagat0399@gmail.com>
2021-07-16 13:09:39 +0530

    buided the basic structure

---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div>
+            <h1>hey this is my Projects </h1>
+        </div>
+    );
+};
+
+export default Projects;
